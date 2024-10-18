a7d5b3538ede67e453f55eafc0114eacfdfaf081
cheerzhang <zhanglenus@gmail.com>
2024-10-18 18:08:13 +0200

    update merge logic

--- main_page.py
+++ main_page.py
@@ -75,7 +75,6 @@ def main():
     if choice == 'Merge':
         st.header('Expected Table')
         df_new = st.session_state.calculate
-        st.dataframe(df_new)
         st.header('Currunt Table')
         df_row = st.session_state.row
         st.dataframe(df_row)
